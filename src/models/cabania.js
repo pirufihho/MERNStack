295aa7368ff8cb83a6f296994007fdfcac671a49
fvalle <fvalle@ticketmundo.com>
2022-05-06 12:13:35 -0300

    //cabanias-MERN implementation

---
+++ src/models/cabania.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const CabaniaSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    imgURI:{type:String,required:false}
+})
+
+module.exports = mongoose.model('Cabania',CabaniaSchema);
