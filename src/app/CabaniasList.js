b9e7fafafcca419479355b59d97feb17c846c8f6
pirufihho <flavionicolasvalle@hotmail.es>
2022-12-11 19:12:26 -0300

    //cng

--- src/app/CabaniasList.js
+++ src/app/CabaniasList.js
@@ -6,7 +6,7 @@ import {
     Link
 } from "react-router-dom";
 import service from '../services/user.service';
-
+import RenderCabania from './RenderCabania';
 
 class CabaniasList extends Component {
     constructor() {
@@ -73,10 +73,11 @@ class CabaniasList extends Component {
         }
     }
 
-    saveFavorite(id) {
+    saveFavorite(data) {
+        console.log(data);
         fetch('/api/favorites', {
             method: 'POST',
-            body: JSON.stringify({ userId: service.getUserId(), cabaniaId: id }),
+            body: JSON.stringify({ userId: service.getUserId(), cabaniaId: data._id }),
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
@@ -129,49 +130,10 @@ class CabaniasList extends Component {
                     </div>
                 </div>
                 {
-                    this.state.cabanias.map(cab => {
+                    this.state.cabanias.map(c => {
                         return (
-                            <div className='container' key={cab._id}>
-                                <div className='row'>
-                                    <div className='col s-6'>
-                                        <div className='card'>
-                                            <div className='card-content' >
-                                                <div className='row'>
-                                                    <h4>{cab.title}</h4>
-                                                </div>
-                                                <div className='row'>
-                                                    {cab.description}
-                                                </div>
-                                                <div className='row'>
-                                                    Province: <b>{cab.province}</b>
-                                                </div>
-                                                <div className='row'>
-                                                    City: <b>{cab.city}</b>
-                                                </div>
-                                                {
-                                                    cab.imgURI != "" && <div className='row'>
-                                                        <img className='imgCabania' src={cab.imgURI} alt={cab.title} />
-                                                    </div>
-                                                }
-                                                <div>
-
-                                                    <Link to={"getCabania/" + cab._id}>Consult</Link>
-
-                                                    {/* <button className='btn light-blue darken-4' >
-                                                            Consult
-                                                        </button> */}
-                                                </div>
-                                                <div>
-                                                    <button className='btn light-blue darken-4' onClick={() => this.saveFavorite(cab._id)} >
-                                                        <i className='material-icons'>favorite
-                                                        </i>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <RenderCabania key={c._id} cabania={c} saveFavorite={this.saveFavorite} 
+                            showDelete={false} showFavorite={true} />
                         )
                     })
                 }
